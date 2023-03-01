289bacae3d4ced1027ea6e9fb75473fa487d88fa
Michael Larios <lariosmichael98@gmail.com>
2023-02-28 17:15:55 -0800

    Added message indicating playlist creation status. Redirect added.

--- src/PandaProgrammers/MusicCollaborationManager/wwwroot/js/generatedplaylists.js
+++ src/PandaProgrammers/MusicCollaborationManager/wwwroot/js/generatedplaylists.js
@@ -26,7 +26,7 @@ $("#save-playlist-btn").click(function () {
         dataAsArray.push(item);
     });
 /*    console.log(dataAsArray);*/
-    
+
     $.ajax({
         method: "POST",
         url: "/api/spotifyauth/savegeneratedplaylist",
@@ -36,13 +36,36 @@ $("#save-playlist-btn").click(function () {
         success: savePlaylist,
         error: errorOnAjax
     });
+
+});
+
+function redirectToGenIndex() {
+    //https://stackoverflow.com/questions/503093/how-do-i-redirect-to-another-webpage - Govind Singh's answer.
+    $(window).attr('location', 'http://localhost:5000/Generator/Index')
+}
+
+
+$("#save-playlist-btn").click(function () {
  
 });
 
 
 function savePlaylist(data) {
     console.log("Result of 'SaveMCMGeneratedPlaylist': " + data);
-    //Perform redirect
+    let text = "The playlist has been saved to your Spotify account";
+
+    if (data === false) {
+        text = "There was an error adding the playlist to your Spotify account";
+    }
+    
+
+    let popUpMsg = `
+    <div class="p-12 bg-gray-800 rounded fixed">
+            <p class="text-white">${text}. You will be redirected shortly...</p>
+    </div>`;
+
+    $("#explanation-title").append(popUpMsg);
+    setTimeout(redirectToGenIndex, 4000);
 }
 
 function errorOnAjax(){
@@ -51,7 +74,10 @@ function errorOnAjax(){
 
 
 $("#discard-playlist-btn").click(function () {
-    console.log("discard button clicked");
-    //Perform redirect
-
+    let msg = `
+    <div class="p-12 bg-gray-800 rounded fixed">
+            <p class="text-white">Playlist has been discarded. You will be redirected shortly...</p>
+    </div>`;
+    $("#explanation-title").append(msg);
+    setTimeout(redirectToGenIndex, 4000);
 });
