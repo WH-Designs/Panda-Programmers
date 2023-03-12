4b8253d0e6871ed32ce3f4618ab1e1b1e72ecb8a
Blake Finnie <bfinnie21@mail.wou.edu>
2023-03-12 16:16:30 -0700

    Further changes

--- src/PandaProgrammers/MusicCollaborationManager/Utilities/GeneratorUtilities.cs
+++ src/PandaProgrammers/MusicCollaborationManager/Utilities/GeneratorUtilities.cs
@@ -17,12 +17,12 @@
             return result;
         }
 
-        public List<string> seedArtists()
-        {
-            Dictionary<string, string> My_dict2 =
-             new Dictionary<string, string>(){
-                                  {"", ""}, {"", ""}, {"", ""}, {"", ""}, {"", ""}, {"", ""}, {"", ""}, {"", ""}, {"", ""}, {"", ""}, {"", ""}, {"", ""}, {"", ""}, {"", ""} };
-        }
+        //public List<string> seedArtists()
+        //{
+        //    Dictionary<string, string> My_dict2 =
+        //     new Dictionary<string, string>(){
+        //                          {"", ""}, {"", ""}, {"", ""}, {"", ""}, {"", ""}, {"", ""}, {"", ""}, {"", ""}, {"", ""}, {"", ""}, {"", ""}, {"", ""}, {"", ""}, {"", ""} };
+        //}
 
         public string getTimeValue(DateTime dt)
         {
