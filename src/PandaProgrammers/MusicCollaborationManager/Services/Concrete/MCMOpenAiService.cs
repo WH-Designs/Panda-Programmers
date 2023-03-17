4822c355f88285815f09e96233ff577d9b9dbe71
Wyatt Haak <wyatt.haak4@gmail.com>
2023-03-16 22:14:00 -0700

    Finished the OpenAi serivce class and each generator now has a input field for a desciption key words that will then generate a custom description for the playlist

---
+++ src/PandaProgrammers/MusicCollaborationManager/Services/Concrete/MCMOpenAiService.cs
@@ -0,0 +1,56 @@
+﻿using MusicCollaborationManager.Services.Abstract;
+using OpenAI.Net;
+using OpenAI.Net.Models.OperationResult;
+using OpenAI.Net.Models.Requests;
+using OpenAI.Net.Models.Responses;
+using OpenAI.Net.Models.Responses.Common;
+
+namespace MusicCollaborationManager.Services.Concrete;
+
+class MCMOpenAiService : IMCMOpenAiService
+{
+    private static IOpenAIService _openAIService { get; set; }
+    public string ErrorMessage { get; set; }
+
+    public OpenAIHttpOperationResult<TextCompletionResponse, OpenAI.Net.Models.Responses.Common.ErrorResponse> response;
+
+    public MCMOpenAiService(IOpenAIService OpenAiService)
+    {
+        _openAIService = OpenAiService;
+    }
+
+    public async Task<string> GetTextResponseFromOpenAiFromUserInput(string UserInput, string Genre)
+    {
+        if (UserInput == null)
+        {
+            string defaultInput = $"Give me a detailed paragraph describing an {Genre} music playlist and don't mention any specific artists.";
+
+            response = await _openAIService.TextCompletion.Get(defaultInput, o =>
+            {
+                o.N = 1;
+                o.MaxTokens = 500;
+            });
+        }
+        else
+        {
+            string InputPlus = $"Using these words {UserInput}, give a detailed description of a {Genre} music playlist and don't mention any specific artists.";
+
+            response = await _openAIService.TextCompletion.Get(InputPlus, o =>
+            {
+                o.N = 1;
+                o.MaxTokens = 500;
+            });
+        }
+
+        if (response.IsSuccess)
+        {
+            string result = String.Join(" ", response.Result.Choices.Select(i => i.Text).ToList());
+
+            return result;
+        }
+        else
+        {
+            return null;
+        }
+    }
+}
