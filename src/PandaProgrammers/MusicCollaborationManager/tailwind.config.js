43a8f2a0478724e76fe3b9ad55dcbf329a8a9631
Wyatt Haak <wyatt.haak4@gmail.com>
2023-04-06 23:14:53 -0700

    navbar now hidden on smaller screen and can be toggled using a button

--- src/PandaProgrammers/MusicCollaborationManager/tailwind.config.js
+++ src/PandaProgrammers/MusicCollaborationManager/tailwind.config.js
@@ -7,23 +7,22 @@ module.exports = {
         './wwwroot/**/*.js'
     ],
     theme: {
-        extend: {},
     },
     plugins: [
         require('tailwindcss-themer')({
             defaultTheme: {
-              // put the default values of any config you want themed
-              // just as if you were to extend tailwind's theme like normal https://tailwindcss.com/docs/theme#extending-the-default-theme
-              extend: {
-                // colors is used here for demonstration purposes
-                colors: {
-                  primary: 'red'
+                // put the default values of any config you want themed
+                // just as if you were to extend tailwind's theme like normal https://tailwindcss.com/docs/theme#extending-the-default-theme
+                extend: {
+                    // colors is used here for demonstration purposes
+                    colors: {
+                        primary: 'red'
+                    }
                 }
-              }
             },
             themes: [
-              {
-                                    // name your theme anything that could be a valid css selector
+                {
+                    // name your theme anything that could be a valid css selector
                     // remember what you named your theme because you will use it as a class to enable the theme
                     name: 'revolution',
                     // put any overrides your theme has here
@@ -38,7 +37,7 @@ module.exports = {
                         }
 
                     }
-                   
+
                 },
                 {
                     // name your theme anything that could be a valid css selector
@@ -67,7 +66,7 @@ module.exports = {
                     // just as if you were to extend tailwind's theme like normal https://tailwindcss.com/docs/theme#extending-the-default-theme
                     extend: {
                         'colors': {
-                             coreback: '#800080',
+                            coreback: '#800080',
                             'primary': 'purple',
                             secondaryback: '#A9A9A9',
                             primback: '#d1d5db',
@@ -114,6 +113,6 @@ module.exports = {
 
                 }
             ]
-          })
-  ]
+        })
+    ]
 }
