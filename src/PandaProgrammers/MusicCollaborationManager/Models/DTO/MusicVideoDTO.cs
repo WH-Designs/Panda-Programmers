2a5d578614f02cea2bc539569183992b2712d363
Michael Larios <lariosmichael98@gmail.com>
2023-04-21 12:20:25 -0700

    Added unit tests.

---
+++ src/PandaProgrammers/MusicCollaborationManager/Models/DTO/MusicVideoDTO.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System.Diagnostics;
+
+namespace MusicCollaborationManager.Models.DTO
+{
+    public class MusicVideoDTO
+    {
+        public string VideoID { get; set; }
+        public string VideoTitle { get; set; }
+        public string ThumbnailURL { get; set; }
+        public string ThumbnailHeight { get; set; }
+        public string ThumbnailWidth { get; set; }
+        public string YouTubeChannelName { get; set; }
+
+        public MusicVideoDTO()
+        {
+        }
+
+        public static IEnumerable<MusicVideoDTO> FromJSON(object? obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/PandaProgrammers/MusicCollaborationManager/Services/Concrete/YouTubeService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Net.Http.Headers;
+using MusicCollaborationManager.Models.DTO;
+using MusicCollaborationManager.Services.Abstract;
+
+namespace MusicCollaborationManager.Services.Concrete
+{
+    public class YouTubeService : IYouTubeService
+    {
+        private readonly string _YTKey;
+        public static readonly HttpClient _httpClient = new HttpClient();
+
+        public YouTubeService(string yTKey)
+        {
+            _YTKey = yTKey;
+        }
+
+        public async Task<string> GetJsonStringFromEndpoint(string uri)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public async Task<IEnumerable<MusicVideoDTO>> GetPopularMusicVideosAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
