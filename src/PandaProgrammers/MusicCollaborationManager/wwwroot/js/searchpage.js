e09064389467f7328c3a896d7fdf91eb6462ac66
Jasper <jasmith17@mail.wou.edu>
2023-04-29 23:49:10 -0700

    added heart button on search

--- src/PandaProgrammers/MusicCollaborationManager/wwwroot/js/searchpage.js
+++ src/PandaProgrammers/MusicCollaborationManager/wwwroot/js/searchpage.js
@@ -108,10 +108,10 @@ function displaySearchResults(data) {
                             <p>${itemType}</p>
                             <p>${itemReleaseDate}</p>
                             <br>
-                            <button class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-white rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800">
-                                <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-gray-800 rounded-md group-hover:bg-opacity-0">
-                                    Like
-                                </span>
+                            <button name='playlistID' class="hover:text-blue-500 classicpanda:text-blacktext" id="playlist-@playlistIndex" aria-current="page" value="@currentPlaylist.Id">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
+                                    </svg>
                             </button>
                         </td>
                     </tr>`
