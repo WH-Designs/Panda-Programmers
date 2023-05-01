5581bd5e84c9a124005f86ad789e59da939cc088
Michael Larios <lariosmichael98@gmail.com>
2023-05-01 07:27:56 -0700

    Created files for unit testing.

---
+++ src/PandaProgrammers/UnitTests/PlaylistPollsRepository_Tests.cs
@@ -0,0 +1,53 @@
+﻿using MusicCollaborationManager.DAL.Abstract;
+using MusicCollaborationManager.DAL.Concrete;
+using MusicCollaborationManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests
+{
+    public class PlaylistPollsRepository_Tests
+    {
+
+        private static readonly string _seedFile = @"..\..\..\DATA\SEED.sql";
+
+        private InMemoryDbHelper<MCMDbContext> _dbHelper = new InMemoryDbHelper<MCMDbContext>(
+            _seedFile,
+            DbPersistence.OneDbPerTest
+        );
+
+
+        //[Test]
+        //public void GetAllOfCommentsForPlaylist_WithAtLeast1Comment_ReturnsAListWithAtLeast1Element() 
+        //{
+        //    // Arrange
+        //    using MCMDbContext context = _dbHelper.GetContext();
+        //    ICommentRepository repo = new CommentRepository(context);
+
+        //    int expectedLength = 2;
+
+        //    List<Comment> expectedList = new List<Comment>
+        //    {
+        //        new Comment {Id = 1,
+        //        Message = "I like this playlist",
+        //        SpotifyId = "0wbYwQItyK648wmeNcqP5z",
+        //        Likes = 10,
+        //        ListenerId = 1},
+
+        //        new Comment {Id = 2,SpotifyId = "0wbYwQItyK648wmeNcqP5z",
+        //        Message = "I dislike this playlist",
+        //        Likes = 20,
+        //        ListenerId = 2}
+        //    };
+
+        //    // Act
+        //    List<Comment> actualList = repo.GetAllOfCommentsForPlaylist("0wbYwQItyK648wmeNcqP5z");
+
+        //    // Assert
+        //    Assert.That(expectedLength, Is.EqualTo(actualList.Count));
+        //}
+    }
+}
---
+++ src/PandaProgrammers/UnitTests/PollsServices_Tests.cs
@@ -0,0 +1,32 @@
+﻿using MusicCollaborationManager.Models.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests
+{
+    //Only testing the parsing methods used by/within each DTO.
+    public class PollsServices_Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void GetTopMusicVideosFromYouTube_WithOneVideo_ShouldParseCorrectly()
+        {
+            //Arrange
+           
+
+
+            //Act
+            
+
+            //Assert
+
+        }
+    }
+}
